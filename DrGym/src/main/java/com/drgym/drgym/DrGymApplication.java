686876f9b75c87e0f6a44ed79cd101f5b6c72710
ksiemion <01187279@pw.edu.pl>
2024-11-07 18:52:04 +0100

    Initialized Spring Project

---
+++ DrGym/src/main/java/com/drgym/drgym/DrGymApplication.java
@@ -0,0 +1,13 @@
+package com.drgym.drgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrGymApplication.class, args);
+	}
+
+}
---
+++ DrGym/src/test/java/com/drgym/drgym/DrGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drgym.drgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
