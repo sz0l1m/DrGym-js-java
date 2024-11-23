6e8debdce613c4b653a9caa2c4c8e70ffe6d3597
Pedziwiatrr <michalozaurus@gmail.com>
2024-11-23 20:15:45 +0100

    implemented StrengthExercise class extending Exercise abstract class

---
+++ DrGym/src/main/java/com/drgym/drgym/model/StrengthExercise.java
@@ -0,0 +1,42 @@
+//author: Michał Pędziwiatr
+
+public class StrengthExercise extends Exercise {
+    private int repetitions;
+    private int weight;
+    private String weightUnit;
+
+    public StrengthExercise(String name, ArrayList<String> musclesWorked, int repetitions, int weight, String weightUnit){
+        super(name, musclesWorked);
+        this.repetitions = repetitions;
+        this.weight = weight;
+        this.weightUnit = weightUnit;
+    }
+
+    public StrengthExercise(String name, ArrayList<String> musclesWorked, int repetitions, int weight){
+        this(name, musclesWorked, repetitions, weight, "kg");
+    }
+
+    public int getRepetitions(){
+        return repetitions;
+    }
+
+    public void setRepetitions(int newRepetitions) {
+        this.repetitions = newRepetitions;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString(){
+        String output = super.toString() + ",\n Repetitions: " + repetitions + ", Weight: " + weight + weightUnit;
+        return output;
+    }
+
+
+}
