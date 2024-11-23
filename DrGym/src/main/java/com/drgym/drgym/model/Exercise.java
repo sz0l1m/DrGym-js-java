ede24111cb5c1a6b691af97b2bbbccaf61960c97
Pedziwiatrr <michalozaurus@gmail.com>
2024-11-23 19:53:04 +0100

    implemented abstract Exercise class

--- DrGym/src/main/java/com/drgym/drgym/model/Exercise.java
+++ DrGym/src/main/java/com/drgym/drgym/model/Exercise.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+
+
 public abstract class Exercise {
+    private String name;
+    private ArrayList<String> musclesWorked;
+
+    public Exercise(String name, ArrayList<String> musclesWorked){
+        this.name = name;
+        if(musclesWorked != null){
+            this.musclesWorked = musclesWorked;
+        }
+        else{
+            this.musclesWorked = new ArrayList<>();
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public ArrayList<String> getMusclesWorked(){
+        return new ArrayList<>(musclesWorked);
+    }
+
+    public void setMusclesWorked(ArrayList<String> newMusclesWorked){
+        musclesWorked = newMusclesWorked;
+    }
+
+    public void addToMusclesWorked(String newMuscle){
+        musclesWorked.add(newMuscle);
+    }
+
+    public void removeFromMusclesWorked(String muscleToRemove){
+        if(musclesWorked.contains(muscleToRemove)){
+            musclesWorked.remove(muscleToRemove);
+            }
+    }
 }
