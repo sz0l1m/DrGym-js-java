eabcb1efe18cb50efcfcbc962f69c153d7f24ca4
Pedziwiatrr <michalozaurus@gmail.com>
2024-11-24 20:37:58 +0100

    separated dummy class to its own file

---
+++ DrGym/src/test/java/com/drgym/drgym/model/ExerciseTestDummy.java
@@ -0,0 +1,11 @@
+//author: Michał Pędziwiatr
+package com.drgym.drgym.model;
+import java.util.ArrayList;
+
+
+public class ExerciseTestDummy extends Exercise {
+
+    public ExerciseTestDummy(String name, ArrayList<String> musclesWorked) {
+        super(name, musclesWorked);
+    }
+}
