f19a4ea43ab04caff43b3145ffe72fb12766a2d9
ksiemion <01187279@pw.edu.pl>
2024-11-30 18:01:41 +0100

    Created classes for posts

--- DrGym/src/main/java/com/drgym/drgym/model/Post.java
+++ DrGym/src/main/java/com/drgym/drgym/model/Post.java
@@ -8,20 +8,29 @@ import java.util.List;
 
 public class Post {
     private Integer id;
+    private Integer authorId;
     private LocalDateTime date;
-    private String title;
     private String content;
-    private Training training;
-    private List<PostComment> comments = new ArrayList<>();
-    private List<PostReaction> reactions = new ArrayList<>();
+    private String title;
+    private Integer trainingId;
+    private List<Integer> comments = new ArrayList<>();
+    private List<Integer> reactions = new ArrayList<>();
 
+    public Post() {}
 
-    public Post() {
-        this.date = LocalDateTime.now();
+    public Post(Integer id, LocalDateTime date, String title, String content) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.content = content;
     }
 
     public Integer getId() {
         return id;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
     }
 
     public LocalDateTime getDate() {
@@ -36,15 +45,63 @@ public class Post {
         return content;
     }
 
-    public Training getTraining() {
-        return training;
+    public Integer getTraining() {
+        return trainingId;
     }
 
-    public List<PostComment> getComments() {
+    public List<Integer> getComments() {
         return comments;
     }
 
-    public List<PostReaction> getReactions() {
+    public List<Integer> getReactions() {
         return reactions;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setTraining(Integer training) {
+        this.trainingId = training;
+    }
+
+    public void setComments(List<Integer> comments) {
+        this.comments = comments;
+    }
+
+    public void setReactions(List<Integer> reactions) {
+        this.reactions = reactions;
+    }
+
+    public void addComment(Integer commentId) {
+        comments.add(commentId);
+    }
+
+    public void addReaction(Integer reactionId) {
+        reactions.add(reactionId);
+    }
+
+    public void removeComment(Integer commentId) {
+        comments.remove(commentId);
+    }
+
+    public void removeReaction(Integer reactionId) {
+        reactions.remove(reactionId);
     }
 }
