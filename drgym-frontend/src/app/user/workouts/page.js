3aaafbfc87c7a416342e40248b8270dc708711dd
mandrysz <01187204@pw.edu.pl>
2024-12-01 22:07:29 +0000

    Added WorkoutCard component

--- drgym-frontend/src/app/user/workouts/page.js
+++ drgym-frontend/src/app/user/workouts/page.js
@@ -1,11 +1,22 @@
-import Typography from '@mui/material/Typography';
+'use client';
+
+import { Box, Typography } from '@mui/material';
+import WorkoutCard from '@/components/WorkoutCard';
+import style from './workouts.module.css';
 
 export default function HomePage() {
   return (
     <>
-      <Typography variant="h4" gutterBottom>
-        Workouts
+      <Typography variant="h5" gutterBottom>
+        My Workouts
       </Typography>
+      <Box className={style.workoutsWrapper}>
+        <WorkoutCard />
+        <WorkoutCard />
+        <WorkoutCard />
+        <WorkoutCard />
+        <WorkoutCard />
+      </Box>
     </>
   );
 }
---
+++ drgym-frontend/src/components/WorkoutCard/WorkoutCard.js
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: 'rotate(0deg)',
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: 'rotate(180deg)',
+      },
+    },
+  ],
+}));
+
+export default function WorkoutCard() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Box sx={{ py: 2 }}>
+      <Card sx={{ maxWidth: 1000 }}>
+        <CardHeader
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Monday 10.11.2024"
+        />
+        <CardContent>
+          <Typography sx={{ marginBottom: 2 }}>
+            <AccessTimeIcon />
+            10.11.2024 13:30
+          </Typography>
+          <Typography>
+            DESCRIPTION This impressive paella is a perfect party dish and a fun
+            meal to cook together with your guests. Add 1 cup of frozen peas
+            along with the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography sx={{ marginBottom: 2 }}>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron
+              and set aside for 10 minutes.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Box>
+  );
+}
