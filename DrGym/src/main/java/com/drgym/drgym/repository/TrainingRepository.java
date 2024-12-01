8f47b4cf0ff25d1fecc00b85d1e0960b8c933f5e
kubaskibicki <kubaskibicki.360@gmail.com>
2024-12-02 00:39:01 +0100

    Added TrainingRepository

---
+++ DrGym/src/main/java/com/drgym/drgym/repository/TrainingRepository.java
@@ -0,0 +1,7 @@
+package com.drgym.drgym.repository;
+
+import com.drgym.drgym.model.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainingRepository extends JpaRepository <Training, Long>{
+}
