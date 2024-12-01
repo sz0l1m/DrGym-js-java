74be2dbac981f4d48eb382399537ba408041c36b
mandrysz <01187204@pw.edu.pl>
2024-12-01 22:05:55 +0000

    Created dateUtils.js in /src/utils

---
+++ drgym-frontend/src/utils/dateUtils.js
@@ -0,0 +1,7 @@
+import { format, parseISO } from 'date-fns';
+
+export const formatDate = (date, pattern = 'MMMM d, yyyy h:mm a') =>
+  format(parseISO(date), pattern);
+
+export const formatRelativeTime = (date) =>
+  formatDistanceToNow(parseISO(date), { addSuffix: true });
