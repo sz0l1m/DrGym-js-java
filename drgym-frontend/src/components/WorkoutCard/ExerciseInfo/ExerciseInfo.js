a74b6594ccb470c9c09e16367b4772511507a8f2
mandrysz <01187204@pw.edu.pl>
2024-12-02 16:57:13 +0000

    Changed icon for cardio exercises

--- drgym-frontend/src/components/WorkoutCard/ExerciseInfo/ExerciseInfo.js
+++ drgym-frontend/src/components/WorkoutCard/ExerciseInfo/ExerciseInfo.js
@@ -2,18 +2,19 @@ import Box from '@mui/material/Box';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import BalanceIcon from '@mui/icons-material/Balance';
 import LoopIcon from '@mui/icons-material/Loop';
+import MonitorHeartOutlinedIcon from '@mui/icons-material/MonitorHeartOutlined';
 import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined';
 import style from './ExerciseInfo.module.css';
 
 export default function ExerciseInfo({ activity }) {
   return (
     <Box className={style.exercise}>
-      <Box sx={{ width: '200px' }} className={style.exerciseElement}>
-        <FitnessCenterIcon />
-        {activity.activity_name}
-      </Box>
       {activity.duration !== undefined ? (
         <>
+          <Box sx={{ width: '200px' }} className={style.exerciseElement}>
+            <MonitorHeartOutlinedIcon />
+            {activity.activity_name}
+          </Box>
           <Box sx={{ width: '100px' }} className={style.exerciseElement}>
             <TimerOutlinedIcon />
             {activity.duration}
@@ -22,6 +23,10 @@ export default function ExerciseInfo({ activity }) {
         </>
       ) : (
         <>
+          <Box sx={{ width: '200px' }} className={style.exerciseElement}>
+            <FitnessCenterIcon />
+            {activity.activity_name}
+          </Box>
           <Box sx={{ width: '100px' }} className={style.exerciseElement}>
             <LoopIcon />
             {activity.sets}
