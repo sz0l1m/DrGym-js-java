a83c6cce54c242a5ef4323e97e7b350a3e600e26
mandrysz <01187204@pw.edu.pl>
2024-12-22 16:31:27 +0000

    Added Login schema

---
+++ drgym-frontend/src/app/login/schema.js
@@ -0,0 +1,33 @@
+import * as yup from 'yup';
+import YupPassword from 'yup-password';
+
+YupPassword(yup);
+
+const LoginSchema = () => {
+  return yup.object().shape({
+    username: yup
+      .string()
+      .email("it's not an email")
+      .max(50, 'maximum 50 characters')
+      .min(5, 'minimum 5 characters')
+      .required("it's required"),
+    password: yup
+      .string()
+      .max(30, 'too long')
+      .min(10, 'too short')
+      .minLowercase(2, 'minimum 2 lowercases')
+      .minUppercase(1, 'minimum 1 uppercase')
+      .minNumbers(1, 'minimum 1 number')
+      .minSymbols(1, 'minimum 1 symbol')
+      .required("it's required"),
+  });
+};
+
+const LoginDefaultValues = () => {
+  return {
+    username: '',
+    password: '',
+  };
+};
+
+export { LoginSchema, LoginDefaultValues };
