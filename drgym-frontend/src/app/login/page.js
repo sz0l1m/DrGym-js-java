a9c6f3d37e1ddd528e9e780ff5edff1e87f43a70
mandrysz <01187204@pw.edu.pl>
2024-12-22 13:11:49 +0000

    Created snackbarProvider

--- drgym-frontend/src/app/login/page.js
+++ drgym-frontend/src/app/login/page.js
@@ -1,11 +1,35 @@
-import Typography from '@mui/material/Typography';
+'use client';
 
-export default function HomePage() {
+import Typography from '@mui/material/Typography';
+import { withSnackbar } from '@/utils/snackbarProvider';
+
+const Login = ({ showAppMessage }) => {
+  const showError = () => {
+    showAppMessage({
+      status: true,
+      text: 'An error occurred!',
+      type: 'error',
+    });
+  };
+
+  const showSuccess = () => {
+    showAppMessage({
+      status: true,
+      text: 'Operation successful!',
+      type: 'success',
+    });
+  };
   return (
     <>
       <Typography variant="h4" gutterBottom>
         Login
       </Typography>
+      <div>
+        <button onClick={showSuccess}>Show Success</button>
+        <button onClick={showError}>Show Error</button>
+      </div>
     </>
   );
-}
+};
+
+export default withSnackbar(Login);
---
+++ drgym-frontend/src/utils/snackbarProvider.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+
+export const withSnackbar = (WrappedComponent) => {
+  const WithSnackbar = (props) => {
+    const [appMessage, setAppMessage] = useState({
+      status: false,
+      text: null,
+      type: null,
+    });
+
+    const handleCloseSnackbar = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+
+      setAppMessage({ status: false, text: '', type: appMessage.type });
+    };
+
+    return (
+      <>
+        <WrappedComponent {...props} showAppMessage={setAppMessage} />
+        <Snackbar
+          open={appMessage?.status}
+          autoHideDuration={6000}
+          onClose={handleCloseSnackbar}
+        >
+          <Alert
+            onClose={handleCloseSnackbar}
+            severity={appMessage?.type}
+            variant="filled"
+            sx={{ width: '100%' }}
+          >
+            {appMessage?.text}
+          </Alert>
+        </Snackbar>
+      </>
+    );
+  };
+
+  WithSnackbar.displayName = `WithSnackbar(${
+    WrappedComponent.displayName || WrappedComponent.name || 'Component'
+  })`;
+
+  return WithSnackbar;
+};
