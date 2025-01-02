8f5d0ace4c19451c690525b7ba8dec0b3106162c
ksiemion <01187279@pw.edu.pl>
2025-01-02 01:31:55 +0100

    updated user class

--- DrGym/src/main/java/com/drgym/drgym/repository/UserRepository.java
+++ DrGym/src/main/java/com/drgym/drgym/repository/UserRepository.java
@@ -2,9 +2,30 @@ package com.drgym.drgym.repository;
 
 import com.drgym.drgym.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    default void verifyEmail(String email) {
+        Optional<User> user = findByEmail(email);
+        user.ifPresent(u -> {
+            u.setVerified(true);
+            save(u);
+        });
+    }
+
+    @Transactional
+    default void resetPassword(String email, String newPassword) {
+        Optional<User> user = findByEmail(email);
+        user.ifPresent(u -> {
+            u.setPassword(newPassword);
+            save(u);
+        });
+    }
 }
