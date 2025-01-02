939e8fb65c92ed6fba338c21ba50d822116a19f9
ksiemion <01187279@pw.edu.pl>
2025-01-02 18:14:44 +0100

    added verification endpoint

--- DrGym/src/main/java/com/drgym/drgym/controller/AuthController.java
+++ DrGym/src/main/java/com/drgym/drgym/controller/AuthController.java
@@ -1,13 +1,13 @@
 package com.drgym.drgym.controller;
 
-import com.drgym.drgym.model.UserLoginRequest;
-import com.drgym.drgym.model.UserForgotPasswordRequest;
-import com.drgym.drgym.model.UserResetPasswordRequest;
-import com.drgym.drgym.model.UserRegistrationRequest;
+import com.drgym.drgym.model.*;
+import com.drgym.drgym.repository.UserRepository;
 import com.drgym.drgym.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,12 +16,18 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserLoginRequest request) {
-        return authService.login(request.getEmail(), request.getPassword());
+    public ResponseEntity<?> login(@RequestBody UserLoginRequest request, HttpServletResponse response) {
+        return authService.login(request.getIdentifier(), request.getPassword(), response);
     }
 
-    @PostMapping("/register")
+    @GetMapping("/verification")
+    public ResponseEntity<?> verify(@RequestParam String email, @RequestParam String token) {
+        return authService.verify(email, token);
+    }
+
+        @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserRegistrationRequest request) {
         return authService.register(request);
     }
