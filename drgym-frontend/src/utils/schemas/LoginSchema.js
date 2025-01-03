ae838e2a4ea326c350bf59b78522cc586a017032
mandrysz <01187204@pw.edu.pl>
2025-01-03 20:58:38 +0000

    Added schema for login by username

--- drgym-frontend/src/utils/schemas/LoginSchema.js
+++ drgym-frontend/src/utils/schemas/LoginSchema.js
@@ -3,13 +3,38 @@ import YupPassword from 'yup-password';
 
 YupPassword(yup);
 
-const LoginSchema = () => {
-  return yup.object().shape({
+const LoginSchema = (loginType) => {
+  const emailSchema = yup.object().shape({
     email: yup
       .string()
       .email("it's not an email")
       .max(50, 'maximum 50 characters')
       .min(5, 'minimum 5 characters')
+      .required("it's required"),
+    username: yup
+      .string()
+      .max(20, 'maximum 20 characters')
+      .min(2, 'minimum 2 characters'),
+    password: yup
+      .string()
+      .max(30, 'too long')
+      .min(10, 'too short')
+      .minLowercase(2, 'minimum 2 lowercases')
+      .minUppercase(1, 'minimum 1 uppercase')
+      .minNumbers(1, 'minimum 1 number')
+      .minSymbols(1, 'minimum 1 symbol')
+      .required("it's required"),
+  });
+  const usernameSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("it's not an email")
+      .max(50, 'maximum 50 characters')
+      .min(5, 'minimum 5 characters'),
+    username: yup
+      .string()
+      .max(20, 'maximum 20 characters')
+      .min(2, 'minimum 2 characters')
       .required("it's required"),
     password: yup
       .string()
@@ -21,11 +46,13 @@ const LoginSchema = () => {
       .minSymbols(1, 'minimum 1 symbol')
       .required("it's required"),
   });
+  return loginType === 'username' ? usernameSchema : emailSchema;
 };
 
 const LoginDefaultValues = () => {
   return {
     email: '',
+    username: '',
     password: '',
   };
 };
