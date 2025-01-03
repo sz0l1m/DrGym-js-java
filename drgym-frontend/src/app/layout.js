3afb266e750299c340335070a02d9bf317c20ac3
mandrysz <01187204@pw.edu.pl>
2025-01-03 17:13:38 +0000

    Added SessionProvider in layout.js

--- drgym-frontend/src/app/layout.js
+++ drgym-frontend/src/app/layout.js
@@ -6,33 +6,36 @@ import CustomAppBar from '@/components/CustomAppBar';
 import theme from '@/styles/theme';
 import '@/app/globals.css';
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import { SessionProvider } from 'next-auth/react';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       <body>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <ProgressBar
-            height="4px"
-            color="#457b9d"
-            options={{ showSpinner: false }}
-            shallowRouting
-          />
-          <CustomAppBar />
-          <Container
-            maxWidth="lg"
-            component="main"
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              my: 14,
-              gap: 4,
-            }}
-          >
-            <Box sx={{ p: 2 }}>{children}</Box>
-          </Container>
-        </ThemeProvider>
+        <SessionProvider session={session}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ProgressBar
+              height="4px"
+              color="#457b9d"
+              options={{ showSpinner: false }}
+              shallowRouting
+            />
+            <CustomAppBar />
+            <Container
+              maxWidth="lg"
+              component="main"
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                my: 14,
+                gap: 4,
+              }}
+            >
+              <Box sx={{ p: 2 }}>{children}</Box>
+            </Container>
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
