33770fe7e5af9899d046bc5ed59476ab04c2c6e0
mandrysz <01187204@pw.edu.pl>
2025-01-10 00:59:42 +0000

    Added request handling in BodyHighlighter

--- drgym-frontend/src/components/BodyHighlighter/BodyHighlighter.js
+++ drgym-frontend/src/components/BodyHighlighter/BodyHighlighter.js
@@ -2,13 +2,36 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { ActivityCalendar } from 'react-activity-calendar';
 import { Tooltip, Typography } from '@mui/material';
-import { calendarData as mockData } from '@/utils/mockData';
 import Grid from '@mui/material/Grid2';
 import axios from 'axios';
 import Model from 'react-body-highlighter';
-import { bodyData as data } from '@/utils/mockData';
+import { bodyData as mockData } from '@/utils/mockData';
 
 const BodyHighlighter = ({ username }) => {
+  const [bodyData, setBodyData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBodyData = async () => {
+      try {
+        setLoading(true);
+        // const response = await axios.get(`/api/body/${username}`);
+        // setBodyData(response.data);
+        setTimeout(() => {
+          setBodyData(mockData);
+          setLoading(false);
+        }, 1500);
+      } catch (error) {
+        setError('Failed to load the data');
+      } finally {
+        // setLoading(false);
+      }
+    };
+
+    fetchBodyData();
+  }, [username]);
+
   const handleClick = React.useCallback(({ muscle, data }) => {
     const { exercises, frequency } = data;
 
@@ -19,16 +42,21 @@ const BodyHighlighter = ({ username }) => {
 
   return (
     <>
+      {error && (
+        <Typography sx={{ mb: 2 }} textAlign="center" color="error">
+          {error}
+        </Typography>
+      )}
       <Grid container justifyContent="center">
         <Model
-          data={data}
+          data={bodyData}
           highlightedColors={['#bbdefb', '#3a88d5', '#0d47a1']}
           style={{ width: '15rem', padding: '1rem' }}
           onClick={handleClick}
         />
         <Model
           type="posterior"
-          data={data}
+          data={bodyData}
           highlightedColors={['#bbdefb', '#3a88d5', '#0d47a1']}
           style={{ width: '15rem', padding: '1rem' }}
           onClick={handleClick}
