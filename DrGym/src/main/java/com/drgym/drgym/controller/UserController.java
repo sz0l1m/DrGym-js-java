612d87d64271e3ad999c5f19a0b87c5f053c22bc
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-11 17:23:07 +0100

    implemented get endpoint for reactions using users query

--- DrGym/src/main/java/com/drgym/drgym/controller/UserController.java
+++ DrGym/src/main/java/com/drgym/drgym/controller/UserController.java
@@ -9,6 +9,7 @@ import com.drgym.drgym.service.ExerciseService;
 import com.drgym.drgym.service.UserService;
 import com.drgym.drgym.service.WorkoutService;
 import com.drgym.drgym.service.PostService;
+import com.drgym.drgym.service.PostReactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +36,8 @@ public class UserController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private PostReactionService postReactionService;
 
     @GetMapping("/{username}")
     public ResponseEntity<?> getUser(@PathVariable String username) {
@@ -148,6 +151,15 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/{username}/posts/{postId}/reactions")
+    public ResponseEntity<?> getReactionsForPost(@PathVariable String username, @PathVariable Long postId) {
+        List<PostReaction> reactions = postReactionService.findByPostId(postId);
+        if (reactions.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(reactions);
     }
 
     public record WorkoutResponse(
