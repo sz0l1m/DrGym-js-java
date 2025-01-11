8a2abb21f9be0b9dc3e49bc587e9e21a93d4e065
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-11 11:47:02 +0100

    connected exerciserepository with get_users_exercises_in_period sql class

--- DrGym/src/main/java/com/drgym/drgym/repository/ExerciseRepository.java
+++ DrGym/src/main/java/com/drgym/drgym/repository/ExerciseRepository.java
@@ -2,6 +2,12 @@ package com.drgym.drgym.repository;
 
 import com.drgym.drgym.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.sql.Clob;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT GET_USERS_EXERCISES_IN_PERIOD(:username, :startDate, :endDate) FROM dual")
+    Clob getExercisesForUserInPeriod(String username, String startDate, String endDate);
 }
