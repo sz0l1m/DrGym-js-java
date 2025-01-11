a7a8912ac0a47e17c36e3347947ae235e5689638
Pedziwiatrr <michalozaurus@gmail.com>
2025-01-11 16:38:33 +0100

    added postReaction service

---
+++ DrGym/src/main/java/com/drgym/drgym/service/PostReactionService.java
@@ -0,0 +1,31 @@
+package com.drgym.drgym.service;
+
+import com.drgym.drgym.model.PostReaction;
+import com.drgym.drgym.repository.PostReactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostReactionService {
+
+    private final PostReactionRepository postReactionRepository;
+
+    @Autowired
+    public PostReactionService(PostReactionRepository postReactionRepository) {
+        this.postReactionRepository = postReactionRepository;
+    }
+
+    public PostReaction addReaction(PostReaction reaction) {
+        return postReactionRepository.save(reaction);
+    }
+
+    public List<PostReaction> getReactionsByPostId(Long postId) {
+        return postReactionRepository.findByPostId(postId);
+    }
+
+    public void removeReaction(Long reactionId) {
+        postReactionRepository.deleteById(reactionId);
+    }
+}
