844f85b84d969dad89976a89fd94404281370c80
ksiemion <01187279@pw.edu.pl>
2025-01-11 23:44:21 +0100

    modified activity class

--- DrGym/src/main/java/com/drgym/drgym/model/Activity.java
+++ DrGym/src/main/java/com/drgym/drgym/model/Activity.java
@@ -3,6 +3,8 @@ package com.drgym.drgym.model;
 import jakarta.persistence.*;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "activities")
@@ -35,27 +37,32 @@ public class Activity {
 
     // getters
 
-    public Long getId() {return id;}
+    public Long getId() { return id; }
 
-    public Long getExerciseId() {return exercise_id;}
+    public Long getExerciseId() { return exercise_id; }
 
-    public Long getReps() {return reps;}
+    public Long getReps() { return reps; }
 
-    public Long getWeight() {return weight;}
+    public Long getWeight() { return weight; }
 
-    public Timestamp getDuration() {return duration;}
+    public Timestamp getDuration() { return duration; }
 
     // setters
 
-    public void setId(Long id_workout) {this.id = id_workout;}
+    public void setId(Long id) { this.id = id; }
 
-    public void setExerciseId(Long exercise_id) {this.exercise_id = exercise_id;}
+    public void setExerciseId(Long exercise_id) { this.exercise_id = exercise_id; }
 
-    public void setReps(Long reps) {this.reps = reps;}
+    public void setReps(Long reps) { this.reps = reps; }
 
-    public void setWeight(Long weight) {this.weight = weight;}
+    public void setWeight(Long weight) { this.weight = weight; }
 
-    public void setDuration(Timestamp duration) {this.duration = duration;}
+    public void setDuration(Timestamp duration) { this.duration = duration; }
+
+    public void setDuration(String duration) {
+        LocalTime localTime = LocalTime.parse(duration);
+        this.duration = Timestamp.valueOf(localTime.atDate(LocalDate.now()));
+    }
 
     public String durationToString() {
         return duration.toString();
