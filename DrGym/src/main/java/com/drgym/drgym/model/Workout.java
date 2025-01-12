885e5011d75c3a0f35081a07d6d8f3216d1e1025
ksiemion <01187279@pw.edu.pl>
2025-01-12 22:36:36 +0100

    updated names in workout class and removed list of activities

--- DrGym/src/main/java/com/drgym/drgym/model/Workout.java
+++ DrGym/src/main/java/com/drgym/drgym/model/Workout.java
@@ -1,9 +1,6 @@
-// author: ksiemion
 package com.drgym.drgym.model;
 
 import java.time.LocalDateTime;
-import java.util.List;
-
 import jakarta.persistence.*;
 
 @Entity
@@ -15,13 +12,13 @@ public class Workout {
     private Long id;
 
     @Column(name = "start_datetime")
-    private LocalDateTime dateStart;
+    private LocalDateTime startDate;
 
     @Column(name = "username")
     private String username;
 
     @Column(name = "end_datetime")
-    private LocalDateTime dateEnd;
+    private LocalDateTime endDate;
 
     @Column(name = "description")
     private String description;
@@ -29,70 +26,30 @@ public class Workout {
     @Column(name = "created_datetime")
     private LocalDateTime dateCreated;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(
-            name = "workout_activities",
-            joinColumns = @JoinColumn(name = "workout_id")
-    )
-    @Column(name = "activity_id")
-    private List<Long> activityIds;
     public Workout() {
     }
 
-    public Workout(LocalDateTime dateStart, String username, LocalDateTime dateEnd, String description, LocalDateTime create_datetime, List<Long> activityIds) {
-        if (dateStart.isAfter(dateEnd)) {
+    public Workout(LocalDateTime startDate, String username, LocalDateTime endDate, String description, LocalDateTime dateCreated) {
+        if (startDate.isAfter(endDate)) {
             throw new IllegalArgumentException("Start date cannot be after end date");
         }
         this.username = username;
-        this.dateStart = dateStart;
-        this.dateEnd = dateEnd;
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.description = description;
-        this.dateCreated = create_datetime;
-        this.activityIds = activityIds;
+        this.dateCreated = dateCreated;
     }
 
-    public Workout(LocalDateTime dateStart, String username, LocalDateTime dateEnd, String description, LocalDateTime create_datetime) {
-        if (dateStart.isAfter(dateEnd)) {
-            throw new IllegalArgumentException("Start date cannot be after end date");
-        }
-        this.username = username;
-        this.dateStart = dateStart;
-        this.dateEnd = dateEnd;
-        this.description = description;
-        this.dateCreated = create_datetime;
-    }
-
-    // getters
-
-    public Long getId() {return id;}
-
-    public LocalDateTime getDateEnd() {return dateEnd;}
-
-    public LocalDateTime getDateStart() {return dateStart;}
-
-    public String getDescription() {return description;}
-
-    public LocalDateTime getDateCreated() {return dateCreated;}
-
-    public List<Long> getActivityIds() {return activityIds;}
-
-    public String getUsername() {return username;}
-
-    // setters
-
-    public void setId(Long id) {this.id = id;}
-
-    public void setDateStart(LocalDateTime dateStart) {this.dateStart = dateStart;}
-
-    public void setDateEnd(LocalDateTime dateEnd) {this.dateEnd = dateEnd;}
-
-    public void setDescription(String description) {this.description = description;}
-
-    public void setDateCreated(LocalDateTime dateCreated) {this.dateCreated = dateCreated;}
-
-    public void setActivityIds(List<Long> activityIds) {this.activityIds = activityIds;}
-
-    public void setUsername(String username) {this.username = username;}
-
+    // getters and setters
+    public Long getId() { return id; }
+    public LocalDateTime getStartDate() { return startDate; }
+    public void setStartDate(LocalDateTime startDate) { this.startDate = startDate; }
+    public LocalDateTime getEndDate() { return endDate; }
+    public void setEndDate(LocalDateTime endDate) { this.endDate = endDate; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public LocalDateTime getDateCreated() { return dateCreated; }
+    public void setDateCreated(LocalDateTime dateCreated) { this.dateCreated = dateCreated; }
 }
-
