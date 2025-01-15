dfe13cbe5e5442e9abc5687239c1ebfbee6ac579
mandrysz <01187204@pw.edu.pl>
2025-01-15 22:10:44 +0000

    Added post title and content

--- drgym-frontend/src/components/Post/Post.js
+++ drgym-frontend/src/components/Post/Post.js
@@ -4,15 +4,21 @@ import Card from '@mui/material/Card';
 import WorkoutInfo from '@/components/WorkoutInfo';
 import { formatRelativeTime } from '@/utils/dateUtils';
 import UserHeader from '@/components/UserHeader';
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 
 export default function Post({ post }) {
   const workout = post.training;
   const realitveStartDate = formatRelativeTime(workout.startDate);
+  console.log('Post', post);
 
   return (
     <>
       <Box sx={{ width: '100%', maxWidth: '1000px', margin: '0 auto', py: 2 }}>
         <Card sx={{ maxWidth: '100%' }}>
+          <Typography sx={{ pt: 2, pl: 2, m: 0 }} variant="h5" gutterBottom>
+            {post.title}
+          </Typography>
           <UserHeader
             username={workout.username}
             subheader={
@@ -20,6 +26,9 @@ export default function Post({ post }) {
               realitveStartDate.slice(1)
             }
           />
+          <Typography sx={{ pb: 1, pl: 2, m: 0 }} variant="body2" gutterBottom>
+            {post.content}
+          </Typography>
           <WorkoutInfo workout={workout} isPost />
         </Card>
       </Box>
