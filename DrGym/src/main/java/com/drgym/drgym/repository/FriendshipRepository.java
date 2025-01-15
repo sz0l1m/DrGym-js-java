edb46bf2e3ecba193d1409c16de86c00f7ae8a61
ksiemion <01187279@pw.edu.pl>
2025-01-15 21:44:11 +0100

    added friendship repository function for friends list

--- DrGym/src/main/java/com/drgym/drgym/repository/FriendshipRepository.java
+++ DrGym/src/main/java/com/drgym/drgym/repository/FriendshipRepository.java
@@ -2,6 +2,7 @@ package com.drgym.drgym.repository;
 
 import com.drgym.drgym.model.Friendship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +10,7 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
     List<Friendship> findByFriend1UsernameOrFriend2Username(String friend1, String friend2);
     boolean existsByFriend1UsernameAndFriend2Username(String friend1, String friend2);
     void deleteByFriend1UsernameAndFriend2Username(String friend1, String friend2);
+    @Query("SELECT f.friend2Username FROM Friendship f WHERE f.friend1Username = :username " +
+            "UNION SELECT f.friend1Username FROM Friendship f WHERE f.friend2Username = :username")
+    List<String> findFriendsUsernamesByUsername(String username);
 }
