fd44ac20dfbce68be6a24726153c187fe89b6187
ksiemion <01187279@pw.edu.pl>
2025-01-15 19:13:49 +0100

    added function for workout repository

--- DrGym/src/main/java/com/drgym/drgym/repository/WorkoutRepository.java
+++ DrGym/src/main/java/com/drgym/drgym/repository/WorkoutRepository.java
@@ -1,10 +1,15 @@
 package com.drgym.drgym.repository;
 
+import com.drgym.drgym.model.Activity;
 import com.drgym.drgym.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface WorkoutRepository extends JpaRepository <Workout, Long>{
     List<Workout> findByUsername(String username);
+    @Query("SELECT a FROM Activity a WHERE a.workoutId = :workoutId")
+    List<Activity> findActivitiesByWorkoutId(@Param("workoutId") Long workoutId);
 }
