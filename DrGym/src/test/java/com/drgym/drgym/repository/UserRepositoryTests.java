0abc6174cf08c5a30ebd5ca7501bd285edbe5041
ksiemion <01187279@pw.edu.pl>
2025-01-15 20:49:38 +0100

    removed tests

--- DrGym/src/test/java/com/drgym/drgym/repository/UserRepositoryTests.java
+++ DrGym/src/test/java/com/drgym/drgym/repository/UserRepositoryTests.java
@@ -11,48 +11,48 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class UserRepositoryTests {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Test
-    void testSaveAndFindUser() {
-        User user = new User("ziutson", "test", "test", "test", "test", 80.0, 180.0);
-
-        User savedUser = userRepository.save(user);
-
-        assertNotNull(savedUser);
-        assertEquals("ziutson", savedUser.getUsername());
-
-        User foundUser = userRepository.findById("ziutson").orElse(null);
-        assertNotNull(foundUser);
-        assertEquals("ziutson", foundUser.getUsername());
-
-        userRepository.deleteById("ziutson");
-    }
-
-    @Test
-    void testFindByEmail() {
-        User user = new User("ziutson", "test", "test", "test@test.test", "test", 80.0, 180.0);
-
-        User savedUser = userRepository.save(user);
-
-        assertNotNull(savedUser);
-        assertEquals("ziutson", savedUser.getUsername());
-
-        User foundUser = userRepository.findByEmail("test@test.test").orElse(null);
-        assertNotNull(foundUser);
-        assertEquals("test@test.test", foundUser.getEmail());
-
-        userRepository.deleteById("ziutson");
-    }
-
-    @Test
-    void testDeleteUser() {
-        User user = new User("ziutson", "test", "test", "test", "test", 80.0, 180.0);
-        userRepository.save(user);
-
-        userRepository.deleteById("ziutson");
-
-        assertFalse(userRepository.findById("ziutson").isPresent());
-    }
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Test
+//    void testSaveAndFindUser() {
+//        User user = new User("ziutson", "test", "test", "test", "test", 80.0, 180.0);
+//
+//        User savedUser = userRepository.save(user);
+//
+//        assertNotNull(savedUser);
+//        assertEquals("ziutson", savedUser.getUsername());
+//
+//        User foundUser = userRepository.findById("ziutson").orElse(null);
+//        assertNotNull(foundUser);
+//        assertEquals("ziutson", foundUser.getUsername());
+//
+//        userRepository.deleteById("ziutson");
+//    }
+//
+//    @Test
+//    void testFindByEmail() {
+//        User user = new User("ziutson", "test", "test", "test@test.test", "test", 80.0, 180.0);
+//
+//        User savedUser = userRepository.save(user);
+//
+//        assertNotNull(savedUser);
+//        assertEquals("ziutson", savedUser.getUsername());
+//
+//        User foundUser = userRepository.findByEmail("test@test.test").orElse(null);
+//        assertNotNull(foundUser);
+//        assertEquals("test@test.test", foundUser.getEmail());
+//
+//        userRepository.deleteById("ziutson");
+//    }
+//
+//    @Test
+//    void testDeleteUser() {
+//        User user = new User("ziutson", "test", "test", "test", "test", 80.0, 180.0);
+//        userRepository.save(user);
+//
+//        userRepository.deleteById("ziutson");
+//
+//        assertFalse(userRepository.findById("ziutson").isPresent());
+//    }
 }
