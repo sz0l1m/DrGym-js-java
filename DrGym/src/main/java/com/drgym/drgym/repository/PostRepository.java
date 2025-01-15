29e827cf8122d5981446c904b62833d88300e044
ksiemion <01187279@pw.edu.pl>
2025-01-15 21:45:20 +0100

    added function in post repository

--- DrGym/src/main/java/com/drgym/drgym/repository/PostRepository.java
+++ DrGym/src/main/java/com/drgym/drgym/repository/PostRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByUsername(String username);
+    List<Post> findByUsernameIn(List<String> usernames);
 }
