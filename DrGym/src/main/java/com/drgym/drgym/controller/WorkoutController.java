8229e8eeb5b5dd9f862327f5f70c9d9706ff663b
ksiemion <01187279@pw.edu.pl>
2025-01-16 12:03:16 +0100

    added exercise type in activities in endpoint

--- DrGym/src/main/java/com/drgym/drgym/controller/WorkoutController.java
+++ DrGym/src/main/java/com/drgym/drgym/controller/WorkoutController.java
@@ -9,9 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,9 +18,6 @@ import java.util.Optional;
 public class WorkoutController {
     @Autowired
     private WorkoutService workoutService;
-
-    @Autowired
-    private ExerciseService exerciseService;
 
     @Autowired
     private UserController userController;
