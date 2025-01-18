73cd29d8cff4a04694e3e990bda255e6c3994ddc
mandrysz <01187204@pw.edu.pl>
2025-01-18 15:15:02 +0000

    Fixed major Calendar bug

--- drgym-frontend/src/components/Calendar/Calendar.js
+++ drgym-frontend/src/components/Calendar/Calendar.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { ActivityCalendar } from 'react-activity-calendar';
 import { Tooltip, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import { calendarData as mockData } from '@/utils/mockData';
 import axiosInstance from '@/utils/axiosInstance';
 import { formatDate } from '@/utils/dateUtils';
@@ -9,15 +10,6 @@ const Calendar = ({ username }) => {
   const [calendarData, setCalendarData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const currentDate = formatDate(new Date().toISOString(), 'yyyy-MM-dd');
-  const oneYearAgo = new Date();
-  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
-  oneYearAgo.setDate(oneYearAgo.getDate() - 1);
-  const oneYearAgoIso = formatDate(oneYearAgo.toISOString(), 'yyyy-MM-dd');
-
-  console.log('oneYearAgoIso', oneYearAgoIso);
-  console.log('currentDate', currentDate);
 
   const calendarTheme = {
     light: ['#ebedf0', '#bbdefb', '#64b5f6', '#1976d2', '#0d47a1'],
@@ -28,74 +20,78 @@ const Calendar = ({ username }) => {
 
   useEffect(() => {
     const fetchCalendarData = async () => {
+      const currentDate = formatDate(new Date().toISOString(), 'yyyy-MM-dd');
+      const yesterdayDate = formatDate(
+        new Date(new Date().setDate(new Date().getDate() - 1)).toISOString(),
+        'yyyy-MM-dd'
+      );
+      const oneYearAgoDate = formatDate(
+        new Date(
+          new Date().setFullYear(new Date().getFullYear() - 1)
+        ).toISOString(),
+        'yyyy-MM-dd'
+      );
+      console.log('currentDate', currentDate);
+      console.log('oneYearAgoDate', oneYearAgoDate);
       try {
         setLoading(true);
-        setTimeout(() => {
-          setCalendarData(
-            setMinCalendarRange(mockData, '2024-01-01', '2024-12-31')
-          );
-          setLoading(false);
-        }, 1000);
-        // const response = await axiosInstance.get(
-        //   `/api/users/mandrysz/daily-exercise-count?startDate=2024-01-01&endDate=2024-12-31`
-        // );
-        // console.log('response', response.data);
-        // const data1 = [
-        //   ...response.data,
-        //   { date: '2025-01-16', count: 0, level: 0 },
-        // ];
-        // const data = [
-        //   ...response.data,
-        //   { date: '2024-01-01', count: 0, level: 0 },
-        //   { date: '2024-12-31', count: 0, level: 0 },
-        // ];
-        // console.log('data', data);
-        // setCalendarData(data);
-        // setCalendarData(
-        //   setMinCalendarRange(response.data, '2024-01-01', '2024-12-31')
-        // );
+        // setTimeout(() => {
+        //   setCalendarData(
+        //     setMinCalendarRange(mockData, '2024-01-01', '2024-12-31')
+        //   );
+        //   setLoading(false);
+        // }, 1000);
+        const response = await axiosInstance.get(
+          `/api/users/${username}/daily-exercise-count?startDate=${oneYearAgoDate}&endDate=${currentDate}`
+        );
+        console.log('response', response.data);
+        setCalendarData(
+          addStartAndEndDate(response.data, oneYearAgoDate, yesterdayDate)
+        );
       } catch (error) {
+        console.error('Failed to load calendar data', error);
         setError('Failed to load calendar data');
       } finally {
-        // setLoading(false);
+        setLoading(false);
       }
     };
 
     fetchCalendarData();
-  }, [username, oneYearAgoIso, currentDate]);
+  }, [username]);
 
-  const setMinCalendarRange = (data, startDate, endDate) => {
-    const dataMap = data.reduce((acc, item) => {
-      acc[item.date] = item;
-      return acc;
-    }, {});
+  function addStartAndEndDate(data, startDate, endDate) {
+    const startObject = { date: startDate, count: 0, level: 0 };
 
-    if (!dataMap[startDate]) {
-      data.push({ date: startDate, count: 0, level: 0 });
+    const updatedData = [startObject, ...data];
+
+    const existingEndObject = data.find((item) => item.date === endDate);
+
+    if (existingEndObject) {
+      updatedData.push({ ...existingEndObject });
+    } else {
+      updatedData.push({ date: endDate, count: 0, level: 0 });
     }
 
-    if (!dataMap[endDate]) {
-      data.push({ date: endDate, count: 0, level: 0 });
-    }
+    return updatedData;
+  }
 
-    return data;
-  };
-
-  const processedCalendarData = [
-    [...calendarData],
-    { date: '2024-12-31', count: 10, level: 3 },
-    { date: '2024-01-01', count: 2, level: 1 },
-  ];
-
-  if (error) return <Typography>{error}</Typography>;
+  if (error)
+    return (
+      <Grid container sx={{ width: '100%' }} justifyContent="center">
+        <Typography>{error}</Typography>
+      </Grid>
+    );
   return (
-    <>
+    <Grid container sx={{ width: '100%' }} justifyContent="center">
       <ActivityCalendar
         data={calendarData}
         theme={calendarTheme}
         maxLevel={4}
         loading={loading}
         weekStart={1}
+        labels={{
+          totalCount: '{{count}} activities in the last year',
+        }}
         renderBlock={(block, activity) => (
           <Tooltip
             key={activity.date}
@@ -110,7 +106,7 @@ const Calendar = ({ username }) => {
           </Tooltip>
         )}
       />
-    </>
+    </Grid>
   );
 };
 
