e8b44c8054589312bad746de1842b4f654d1a9c8
mandrysz <01187204@pw.edu.pl>
2025-01-19 21:25:18 +0000

    Fixed styles in ActivityInfo

--- drgym-frontend/src/components/ActivityInfo/ActivityInfo.js
+++ drgym-frontend/src/components/ActivityInfo/ActivityInfo.js
@@ -6,23 +6,39 @@ import LoopIcon from '@mui/icons-material/Loop';
 import MonitorHeartOutlinedIcon from '@mui/icons-material/MonitorHeartOutlined';
 import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined';
 import SportsGymnasticsIcon from '@mui/icons-material/SportsGymnastics';
+import Grid from '@mui/material/Grid2';
 import style from './ActivityInfo.module.css';
 
 export default function ActivityInfo({ activity }) {
   const type = activity.exerciseType;
 
   return (
-    <Box className={style.activity}>
+    <Grid
+      container
+      flexWrap="wrap"
+      sx={{
+        justifyContent: { xs: 'flex-start', sm: 'space-between' },
+        maxWidth: '600px',
+        mb: 3,
+        mr: 3,
+      }}
+    >
       {type === 'C' ? (
         <>
-          <Box sx={{ width: '200px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: { xs: '100%', sm: '200px' } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Cardio exercise">
               <MonitorHeartOutlinedIcon />
             </Tooltip>
             {activity.exerciseName}
           </Box>
 
-          <Box sx={{ width: '100px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: '100px', pt: { xs: 2, sm: 0 } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Duration">
               <TimerOutlinedIcon />
             </Tooltip>
@@ -32,21 +48,30 @@ export default function ActivityInfo({ activity }) {
         </>
       ) : type === 'S' ? (
         <>
-          <Box sx={{ width: '200px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: { xs: '100%', sm: '200px' } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Strength exercise">
               <FitnessCenterIcon />
             </Tooltip>
             {activity.exerciseName}
           </Box>
 
-          <Box sx={{ width: '100px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: '100px', pt: { xs: 2, sm: 0 } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Number of reps">
               <LoopIcon />
             </Tooltip>
             {activity.reps}
           </Box>
 
-          <Box sx={{ width: '100px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: '100px', pt: { xs: 2, sm: 0 } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Weight [kg]">
               <BalanceIcon sx={{ ml: 4 }} />
             </Tooltip>
@@ -55,21 +80,30 @@ export default function ActivityInfo({ activity }) {
         </>
       ) : (
         <>
-          <Box sx={{ width: '200px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: { xs: '100%', sm: '200px' } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Crossfit exercise">
               <SportsGymnasticsIcon />
             </Tooltip>
             {activity.exerciseName}
           </Box>
 
-          <Box sx={{ width: '100px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: '100px', pt: { xs: 2, sm: 0 } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Duration">
               <TimerOutlinedIcon />
             </Tooltip>
             {activity.duration}
           </Box>
 
-          <Box sx={{ width: '100px' }} className={style.activityElement}>
+          <Box
+            sx={{ width: '100px', pt: { xs: 2, sm: 0 } }}
+            className={style.activityElement}
+          >
             <Tooltip title="Weight [kg]">
               <BalanceIcon sx={{ ml: 4 }} />
             </Tooltip>
@@ -77,6 +111,6 @@ export default function ActivityInfo({ activity }) {
           </Box>
         </>
       )}
-    </Box>
+    </Grid>
   );
 }
