653f8f7b6058938d8b887f99eb1e8f7c9f8d9561
mandrysz <01187204@pw.edu.pl>
2025-01-21 19:26:58 +0000

    Displayed future and past workouts separetely

--- drgym-frontend/src/app/user/[user]/workouts/page.js
+++ drgym-frontend/src/app/user/[user]/workouts/page.js
@@ -9,13 +9,17 @@ import WorkoutForm from '@/components/WorkoutForm';
 import { withSnackbar } from '@/utils/snackbarProvider';
 import SkeletonCard from '@/components/SkeletonCard';
 import Grid from '@mui/material/Grid2';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { getUsername } from '@/utils/localStorage';
 
 const Workouts = ({ showAppMessage }) => {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [workoutsData, setWorkoutsData] = useState([]);
+  const [allWorkouts, setAllWorkouts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [fromFuture, setFromFuture] = useState(true);
   const username = getUsername();
 
   const fetchWorkouts = useCallback(async () => {
@@ -24,7 +28,9 @@ const Workouts = ({ showAppMessage }) => {
       const response = await axiosInstance.get(
         `/api/users/${username}/workouts`
       );
-      setWorkoutsData(response.data);
+      setAllWorkouts(response.data);
+      setFromFuture(true);
+      setWorkoutsData(response.data.futureWorkouts);
     } catch (err) {
       setError('fetch worokuts', err.message);
       showAppMessage({
@@ -45,15 +51,35 @@ const Workouts = ({ showAppMessage }) => {
     setWorkoutsData((prev) => prev.filter((workout) => workout.id !== id));
   };
 
+  const handleTypeChange = (event, newType) => {
+    if (newType === 'future') {
+      setWorkoutsData(allWorkouts.futureWorkouts);
+      setFromFuture(true);
+    } else {
+      setWorkoutsData(allWorkouts.pastWorkouts);
+      setFromFuture(false);
+    }
+  };
+
   if (error) return <Typography>Error: {error}</Typography>;
   return (
     <>
-      <Box
-        sx={{ display: 'flex', justifyContent: 'space-between', pt: 1, pb: 2 }}
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ pt: 1, pb: 2 }}
       >
-        <Typography variant="h5" gutterBottom>
-          My Workouts
-        </Typography>
+        <ToggleButtonGroup
+          color="info"
+          value={fromFuture ? 'future' : 'past'}
+          exclusive
+          onChange={handleTypeChange}
+          aria-label="Which workouts to show"
+        >
+          <ToggleButton value="future">Future workouts</ToggleButton>
+          <ToggleButton value="past">Old workouts</ToggleButton>
+        </ToggleButtonGroup>
         <Button
           variant="contained"
           startIcon={<AddIcon />}
@@ -61,7 +87,7 @@ const Workouts = ({ showAppMessage }) => {
         >
           Add workout
         </Button>
-      </Box>
+      </Grid>
       <Grid container direction="column" alignItems="center">
         {loading ? (
           Array.from({ length: 3 }).map((_, index) => (
@@ -69,7 +95,9 @@ const Workouts = ({ showAppMessage }) => {
           ))
         ) : workoutsData.length === 0 ? (
           <Typography variant="h6">
-            You have not added any workouts yet.
+            {fromFuture
+              ? "You don't have any workouts planned"
+              : "You don't have any past workouts"}
           </Typography>
         ) : (
           workoutsData.map((workout) => (
