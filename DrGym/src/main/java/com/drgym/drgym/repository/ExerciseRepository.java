4038fc001d62ee4adfb1bb2b50d3089b51dfca7b
ksiemion <01187279@pw.edu.pl>
2025-01-22 18:19:44 +0100

    added exercise repository function for ranking

--- DrGym/src/main/java/com/drgym/drgym/repository/ExerciseRepository.java
+++ DrGym/src/main/java/com/drgym/drgym/repository/ExerciseRepository.java
@@ -14,4 +14,7 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
     @Query(nativeQuery = true, value = "SELECT GET_USER_DAILY_EXERCISE_COUNT(:username, :startDate, :endDate) FROM dual")
     Clob getUserDailyExerciseCount(String username, String startDate, String endDate);
+
+    @Query(nativeQuery = true, value = "SELECT GET_EXERCISE_RANKING(:username, :exerciseId) FROM dual")
+    Clob getExerciseRanking(@Param("username") String username, @Param("exerciseId") int exerciseId);
 }
