6089fffde3abf048face86ea8595c18a0b6ce55f
mandrysz <01187204@pw.edu.pl>
2025-01-24 10:14:29 +0000

    FIxed YouTubePlayer for smaller widths

--- drgym-frontend/src/components/YouTubePlayer/YouTubePlayer.js
+++ drgym-frontend/src/components/YouTubePlayer/YouTubePlayer.js
@@ -1,25 +1,54 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import YouTube from 'react-youtube';
 import Skeleton from '@mui/material/Skeleton';
 import Box from '@mui/material/Box';
 
 const YouTubePlayer = ({ videoId }) => {
   const [loading, setLoading] = useState(true);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const containerElement = containerRef.current;
+    const handleResize = () => {
+      if (containerElement) {
+        const width = containerElement.offsetWidth;
+        const height = width * (9 / 16);
+        setDimensions({ width, height });
+      }
+    };
+
+    const resizeObserver = new ResizeObserver(() => handleResize());
+    if (containerElement) {
+      resizeObserver.observe(containerElement);
+    }
+
+    handleResize();
+
+    return () => {
+      if (containerElement) {
+        resizeObserver.unobserve(containerElement);
+      }
+    };
+  }, []);
 
   const opts = {
+    height: dimensions.height,
+    width: dimensions.width,
     playerVars: {
       autoplay: 0,
     },
   };
 
-  const onReady = (event) => {
+  const onReady = () => {
     setLoading(false);
   };
 
   return (
     <Box
+      ref={containerRef}
       sx={{
         position: 'relative',
         width: '100%',
@@ -53,7 +82,9 @@ const YouTubePlayer = ({ videoId }) => {
           height: '100%',
         }}
       >
-        <YouTube videoId={videoId} opts={opts} onReady={onReady} />
+        {dimensions.width > 0 && (
+          <YouTube videoId={videoId} opts={opts} onReady={onReady} />
+        )}
       </Box>
     </Box>
   );
