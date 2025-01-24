208b5f249e55861d6e72357e417df0fdfcb5d434
mandrysz <01187204@pw.edu.pl>
2025-01-24 01:28:27 +0000

    Added loading state for YouTubePlayer

--- drgym-frontend/src/components/YouTubePlayer/YouTubePlayer.js
+++ drgym-frontend/src/components/YouTubePlayer/YouTubePlayer.js
@@ -1,22 +1,48 @@
-'user client';
+'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import YouTube from 'react-youtube';
+import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
 
 const YouTubePlayer = ({ videoId }) => {
+  const [loading, setLoading] = useState(true);
+
   const opts = {
-    height: '390',
-    width: '640',
+    height: '390px',
+    width: '640px',
     playerVars: {
       autoplay: 0,
     },
   };
 
   const onReady = (event) => {
-    // event.target.pauseVideo();
+    setLoading(false);
   };
 
-  return <YouTube videoId={videoId} opts={opts} onReady={onReady} />;
+  return (
+    <Box sx={{ position: 'relative', width: opts.width, height: opts.height }}>
+      {loading && (
+        <Skeleton
+          variant="rectangular"
+          width={opts.width}
+          height={opts.height}
+          animation="wave"
+        />
+      )}
+
+      <Box
+        sx={{
+          display: loading ? 'none' : 'block',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      >
+        <YouTube videoId={videoId} opts={opts} onReady={onReady} />
+      </Box>
+    </Box>
+  );
 };
 
 export default YouTubePlayer;
